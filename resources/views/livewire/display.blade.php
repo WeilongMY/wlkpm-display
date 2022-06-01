24864127880e5ab43055d561cee6d8ec19625868
Wan Amra <wanamra@gmail.com>
2022-06-01 10:15:58 +0800

    tambah checking database connection

--- resources/views/livewire/display.blade.php
+++ resources/views/livewire/display.blade.php
@@ -1,7 +1,7 @@
 <div>
     <div class="flex justify-center" wire:poll.3s>
         <p class="absolute top-1/4 text-white text-7xl font-medium text-center uppercase">{!! $greeting !!}</p>
-        <p class="absolute top-1/3 text-green-400 text-15xl font-bold right-48">{{ $count }}</p>
+        <p class="absolute top-1/3 text-green-400 text-15xl font-bold right-44">{{ $count }}</p>
 
         @if($_SERVER['REMOTE_ADDR'] == '192.168.1.192')
             <div class="absolute bottom-1/4">
@@ -9,8 +9,6 @@
                 <p class="text-white text-7xl font-medium text-center uppercase mt-8">{!! $info !!}</p>
             </div>
         @endif
-
-{{--        <div class="absolute bottom-1/3 text-white text-5xl font-bold text-center">WE CAPTURED YOUR CARPLATE! <br> PLEASE PAY AT EXIT.</div>--}}
     </div>
     <div class="absolute bottom-0 right-6">
         <div class="flex justify-end">

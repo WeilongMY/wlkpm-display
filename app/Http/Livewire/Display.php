27919509ff73ddca58aae23dde50801b2cb4a485
Wan Amra <wanamra@gmail.com>
2022-06-20 10:12:12 +0800

    tambah checking database connection

--- app/Http/Livewire/Display.php
+++ app/Http/Livewire/Display.php
@@ -2,12 +2,9 @@
 
 namespace App\Http\Livewire;
 
-use App\Integrations\PrayerTimes\PrayerTimes;
 use App\Models\Parking1;
 use App\Models\Parkgroup;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
+use Carbon\Carbon;
 use Livewire\Component;
 
 class Display extends Component
@@ -17,6 +14,28 @@ class Display extends Component
     public $message;
     public $info;
     public $count;
+    public $datetime;
+
+    public function getTotalLots($groupId)
+    {
+        return Parkgroup::where('groupid', $groupId)->first();
+    }
+
+    public function getTotalOccupied($sids)
+    {
+        return Parking1::where('state', 0)
+            ->where('date1',date('Y-m-d'))
+            ->where(function ($query) use ($sids) {
+                return $query->whereIn('sid', $sids);
+            })
+            ->where(function ($query) {
+                return $query->where('can', 'LIKE', "M-%%")
+                    ->orWhere('can', 'LIKE', "I-%%")
+                    ->orWhere('can', 'LIKE', "U0%%")
+                    ->orWhere('can', 'LIKE', "E-%%")
+                    ->orWhere('can', 'LIKE', "V-%%");
+            })->count('sid');
+    }
 
     public function render()
     {
@@ -24,95 +43,40 @@ class Display extends Component
         $this->message = config('display.message');
         $this->info = config('display.info');
 
-        try {
-            while (1) {
-                $connect = DB::connection()->getPdo();
-
-                if ($connect) {
-                    break;
-                }
-            }
-        } catch (\Exception $e) {
-            die("Could not connect to the database.  Please check your configuration. error:" . $e );
-        }
-
         switch($_SERVER['REMOTE_ADDR']){
             // South Car(gid 4). 78,79,80,81
             case "192.168.1.190":
-                $totalLots = Parkgroup::where('groupid',4)->first();
-                $totalOccupied = Parking1::where('state', 0)
-                    ->where('date1',date('Y-m-d'))
-                    ->where(function ($query) {
-                        return $query->where('sid', 78)
-                            ->orWhere('sid', 79)
-                            ->orWhere('sid', 80)
-                            ->orWhere('sid', 81);
-                    })
-                    ->where(function ($query) {
-                        return $query->where('can', 'LIKE', "M-%%")
-                            ->orWhere('can', 'LIKE', "I-%%")
-                            ->orWhere('can', 'LIKE', "U0%%")
-                            ->orWhere('can', 'LIKE', "E-%%")
-                            ->orWhere('can', 'LIKE', "V-%%");
-                    })->count('sid');
+                $totalLots = $this->getTotalLots(4);
+                $sids = ['78', '79', '80', '81'];
+                $totalOccupied = $this->getTotalOccupied($sids);
                 break;
             // Rooftop Car(gid 1). 34,35,82,83
             case "192.168.1.191":
-                $totalLots = Parkgroup::where('groupid',1)->first();
-                $totalOccupied = Parking1::where('state', 0)
-                    ->where('date1',date('Y-m-d'))
-                    ->where(function ($query) {
-                        return $query->where('sid', 34)
-                            ->orWhere('sid', 35)
-                            ->orWhere('sid', 82)
-                            ->orWhere('sid', 83);
-                    })
-                    ->where(function ($query) {
-                        return $query->where('can', 'LIKE', "M-%%")
-                            ->orWhere('can', 'LIKE', "I-%%")
-                            ->orWhere('can', 'LIKE', "U0%%")
-                            ->orWhere('can', 'LIKE', "E-%%")
-                            ->orWhere('can', 'LIKE', "V-%%");
-                    })->count('sid');
+                $totalLots = $this->getTotalLots(1);
+                $sids = ['34', '35', '82', '83'];
+                $totalOccupied = $this->getTotalOccupied($sids);
                 break;
             // North Car(gid 2). 30,31,32,33
             case "192.168.1.192":
-                $totalLots = Parkgroup::where('groupid',2)->first();
-                $totalOccupied = Parking1::where('state', 0)
-                    ->where('date1',date('Y-m-d'))
-                    ->where(function ($query) {
-                        return $query->where('sid', 30)
-                            ->orWhere('sid', 31)
-                            ->orWhere('sid', 32)
-                            ->orWhere('sid', 33);
-                    })
-                    ->where(function ($query) {
-                        return $query->where('can', 'LIKE', "M-%%")
-                            ->orWhere('can', 'LIKE', "I-%%")
-                            ->orWhere('can', 'LIKE', "U0%%")
-                            ->orWhere('can', 'LIKE', "E-%%")
-                            ->orWhere('can', 'LIKE', "V-%%");
-                    })->count('sid');
+                $totalLots = $this->getTotalLots(2);
+                $sids = ['30', '31', '32', '33'];
+                $totalOccupied = $this->getTotalOccupied($sids);
                 break;
             default:
-                $totalLots = Parkgroup::where('groupid',0)->first();
-                $totalOccupied = Parking1::where('state', 0)
-                    ->where('date1',date('Y-m-d'))
-                    ->where(function ($query) {
-                        return $query->where('can', 'LIKE', "M-%%")
-                            ->orWhere('can', 'LIKE', "I-%%")
-                            ->orWhere('can', 'LIKE', "U0%%")
-                            ->orWhere('can', 'LIKE', "E-%%")
-                            ->orWhere('can', 'LIKE', "V-%%");
-                    })->count('sid');
+                $totalLots = $this->getTotalLots(0);
+                $sids = [0];
+                $totalOccupied = $this->getTotalOccupied($sids);
         }
 
         $availableLots = $totalLots->LIMIT1 - $totalOccupied;
+
         if ($availableLots < 0) {
             $this->count = 0;
         } else {
             $this->count = $availableLots;
         }
+
+        $this->datetime = Carbon::now()->format('l, d M Y')." | ".Carbon::now()->format('H:i');
 
         return view('livewire.display');
     }
--- resources/views/livewire/display.blade.php
+++ resources/views/livewire/display.blade.php
@@ -1,16 +1,16 @@
 <div>
     <div class="flex justify-center" wire:poll.3s>
-        <p class="absolute top-1/4 text-white text-7xl font-medium text-center uppercase">{!! $greeting !!}</p>
+        <p class="absolute top-1/4 text-white text-7xl font-medium text-center uppercase">{{ $greeting }}</p>
         <p class="absolute top-1/3 text-green-400 text-15xl font-bold right-36">{{ $count }}</p>
 
         <div class="absolute bottom-1/4">
-            <p class="text-white text-7xl font-medium text-center uppercase">{!! $message !!}</p><br>
-            <p class="text-white text-7xl font-medium text-center uppercase mt-8">{!! $info !!}</p>
+            <p class="text-white text-7xl font-medium text-center uppercase">{{ $message }}</p><br>
+            <p class="text-white text-7xl font-medium text-center uppercase mt-8">{{ $info }}</p>
         </div>
     </div>
     <div class="absolute bottom-0 right-6">
         <div class="flex justify-end">
-            <p class="p-6 text-black text-3xl font-bold text-right">{{ \Carbon\Carbon::now()->format('l, d M Y') }} | {{ \Carbon\Carbon::now()->format('H:i') }} | {{ Alkoumi\LaravelHijriDate\Hijri::Date('l ، j F ، Y', Carbon\Carbon::now()) }}</p>
+            <p class="p-6 text-black text-3xl font-bold text-right">{{ $datetime }}</p>
         </div>
     </div>
 </div>

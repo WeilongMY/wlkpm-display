cae19255a469c265af35a4434896857145e71fda
Wan Amra <wanamra@gmail.com>
2022-07-08 14:27:29 +0800

    tambah checking database connection

--- app/Http/Livewire/Display.php
+++ app/Http/Livewire/Display.php
@@ -5,6 +5,8 @@ namespace App\Http\Livewire;
 use App\Models\Parking1;
 use App\Models\Parkgroup;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 use Livewire\Component;
 
 class Display extends Component
@@ -13,7 +15,7 @@ class Display extends Component
     public $greeting;
     public $message;
     public $info;
-    public $count;
+    public $count = 0;
     public $datetime;
 
     public function getTotalLots($groupId)
@@ -43,37 +45,43 @@ class Display extends Component
         $this->message = config('display.message');
         $this->info = config('display.info');
 
-        switch($_SERVER['REMOTE_ADDR']){
-            // South Car(gid 4). 78,79,80,81
-            case "192.168.1.190":
-                $totalLots = $this->getTotalLots(4);
-                $sids = ['78', '79', '80', '81'];
-                $totalOccupied = $this->getTotalOccupied($sids);
-                break;
-            // Rooftop Car(gid 1). 34,35,82,83
-            case "192.168.1.191":
-                $totalLots = $this->getTotalLots(1);
-                $sids = ['34', '35', '82', '83'];
-                $totalOccupied = $this->getTotalOccupied($sids);
-                break;
-            // North Car(gid 2). 30,31,32,33
-            case "192.168.1.192":
-                $totalLots = $this->getTotalLots(2);
-                $sids = ['30', '31', '32', '33'];
-                $totalOccupied = $this->getTotalOccupied($sids);
-                break;
-            default:
-                $totalLots = $this->getTotalLots(0);
-                $sids = [0];
-                $totalOccupied = $this->getTotalOccupied($sids);
-        }
+        try {
+            DB::connection()->getPdo();
 
-        $availableLots = $totalLots->LIMIT1 - $totalOccupied;
+            switch(request()->ip()){
+                // South Car(gid 4). 78,79,80,81
+                case "192.168.1.190":
+                    $totalLots = $this->getTotalLots(4);
+                    $sids = ['78', '79', '80', '81'];
+                    $totalOccupied = $this->getTotalOccupied($sids);
+                    break;
+                // Rooftop Car(gid 1). 34,35,82,83
+                case "192.168.1.191":
+                    $totalLots = $this->getTotalLots(1);
+                    $sids = ['34', '35', '82', '83'];
+                    $totalOccupied = $this->getTotalOccupied($sids);
+                    break;
+                // North Car(gid 2). 30,31,32,33
+                case "192.168.1.192":
+                    $totalLots = $this->getTotalLots(2);
+                    $sids = ['30', '31', '32', '33'];
+                    $totalOccupied = $this->getTotalOccupied($sids);
+                    break;
+                default:
+                    $totalLots = $this->getTotalLots(0);
+                    $sids = [0];
+                    $totalOccupied = $this->getTotalOccupied($sids);
+            }
 
-        if ($availableLots < 0) {
-            $this->count = 0;
-        } else {
-            $this->count = $availableLots;
+            $availableLots = $totalLots->LIMIT1 - $totalOccupied;
+
+            if ($availableLots < 0) {
+                $this->count = 0;
+            } else {
+                $this->count = $availableLots;
+            }
+        } catch (\Exception $e) {
+            Log::error("Database error ". $e);
         }
 
         $this->datetime = Carbon::now()->format('l, d M Y')." | ".Carbon::now()->format('H:i');
